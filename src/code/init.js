0299a342683f0eb0921547f3ee757ad7f04c005b
WebFan37 <118756551+WebFan37@users.noreply.github.com>
2025-04-12 15:21:57 -0400

    Setup FireStore et utilisateur

--- src/code/init.js
+++ src/code/init.js
@@ -1,6 +1,8 @@
 import objetConfig from './config.js';
 import {initializeApp} from 'firebase/app';
 import {getAuth, GoogleAuthProvider} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore'
+import {getStorage} from 'firebase/storage'
 
 // Initialiser les services de Firebase
 export const appli = initializeApp(objetConfig);
@@ -12,3 +14,12 @@ export const firebaseAuth = getAuth(appli);
 export const googleProvider = new  GoogleAuthProvider();
 
 //Initialiser les autres services Firebase si nécessaire
+
+//Firebase
+export const bd = getFirestore(appli)
+
+//1 collection utilise
+export const collUtil = "signet-utilisateur"
+
+//2 collection dossier
+export const collDossier = "dossiers"
--- src/code/utilisateur.js
+++ src/code/utilisateur.js
@@ -1,5 +1,6 @@
-import {firebaseAuth, googleProvider} from './init.js';
+import {firebaseAuth, googleProvider, bd, collUtil} from './init.js';
 import { signInWithPopup, signOut, onAuthStateChanged} from 'firebase/auth';
+import { setDoc, doc, serverTimestamp} from 'firebase/firestore';
 
 
 export function connexion(){
@@ -13,6 +14,22 @@ export function deconnexion(){
 
 export function observerEtatConnexion(mutateurUtilisateur){
     onAuthStateChanged(firebaseAuth, 
-       (utilisateur) => mutateurUtilisateur(utilisateur) // on met a jour l'etat de l'utilisateur
+       (utilisateur) => {
+        
+        if (utilisateur){
+            //Si true, enregistrer les donnes de cette utilisateurs dans cette bd
+            const referenceUtilisateur = doc(bd, collUtil, utilisateur.uid)
+
+            setDoc(referenceUtilisateur, {
+                nom: utilisateur.displayName,
+                avatar: utilisateur.photoURL,
+                courriel: utilisateur.email,
+                dateCreation: serverTimestamp()
+            }, {merge: true})
+
+        }
+        mutateurUtilisateur(utilisateur)
+    
+    } // on met a jour l'etat de l'utilisateur
     )
 }
